c2c5ecd32b165313d5afc705778a36ca41663dc4
DhananiPavan <dhananipavan369@gmail.com>
2025-08-23 19:59:04 +0530

    Time: 3 ms (76.20%) | Memory: 50.8 MB (9.08%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,28 +1,50 @@
 class Solution {
-    public List<Integer> majorityElement(int[] v) {
-      int n = v.length; //size of the array
-        List<Integer> ls = new ArrayList<>(); // list of answers
+    public List<Integer> majorityElement(int[] n) {
+        List<Integer> l=new ArrayList();
+        int c1=0,c2=0;
+        int e1 = Integer.MIN_VALUE;
+        int e2 = Integer.MIN_VALUE;
+        for(int i=0;i<n.length;i++){
+            if(c1==0&&n[i]!=e2){
+                c1++;
+                e1=n[i];
 
-        //declaring a map:
-        HashMap<Integer, Integer> mpp = new HashMap<>();
+            }else if(c2==0&&n[i]!=e1){
+                 c2++;
+                e2=n[i];
 
-        // least occurrence of the majority element:
-        int mini = (int)(n / 3) + 1;
-
-        //storing the elements with its occurnce:
-        for (int i = 0; i < n; i++) {
-            int value = mpp.getOrDefault(v[i], 0);
-            mpp.put(v[i], value + 1);
-
-            //checking if v[i] is
-            // the majority element:
-            if (mpp.get(v[i]) == mini) {
-                ls.add(v[i]);
             }
-            if (ls.size() == 2) break;
-        }
 
-        return ls;
+            else if(e1==n[i]){
+                c1++;
+            }
+            else if(e2==n[i]){
+                c2++;
+            }
+            else{
+                c1--;c2--;
+            }
+
+
+        }
+        c1=0;c2=0;
+        int min =(int)(n.length/3)+1;
+        for(int i=0;i<n.length;i++)
+        {
+            if(e1==n[i]){
+                c1++;
+            }
+            if(e2==n[i]){
+                c2++;
+            }
+        }
+        if(c1>=min){
+            l.add(e1);
+        }
+        if(c2>=min){
+            l.add(e2);
+        }
+        return l;
 
         
     }
