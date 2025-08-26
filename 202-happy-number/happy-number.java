42c958931db3849aa8d0e52db510f04fcf599959
DhananiPavan <dhananipavan369@gmail.com>
2025-08-26 21:58:59 +0530

    Time: 2 ms (18.00%) | Memory: 41 MB (28.34%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        java.util.HashSet<Integer> seen = new java.util.HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = sumOfSquares(n);
+        }
+        return n == 1;
+    }
+
+    private int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+}
