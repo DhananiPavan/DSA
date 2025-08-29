aa8bd51fb0d8ebd8fc6fc3bcd1fa4a3a1f8b855d
DhananiPavan <dhananipavan369@gmail.com>
2025-08-29 20:50:30 +0530

    Time: 8 ms (85.45%) | Memory: 47.3 MB (6.19%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] merge(int[][] arr) {
+          int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });// it is ctr a anonymas class that implents the comparator interface ,it take 1st ele in ec row 
+        // take list 
+        List<List<Integer>> ans=new ArrayList();
+
+        for(int i=0;i<n;i++){
+            if(ans.isEmpty() || ans.get(ans.size()-1).get(1) < arr[i][0]){
+                ans.add(Arrays.asList(arr[i][0],arr[i][1]));
+
+            }
+            else{
+               ans.get(ans.size()-1).set(1,Math.max(ans.get(ans.size()-1).get(1),arr[i][1]));
+            }
+        }
+        // Convert List<List<Integer>> to int[][]
+        int[][] res = new int[ans.size()][2];
+       for(int i = 0; i < ans.size(); i++) {
+                     res[i][0] = ans.get(i).get(0);
+                     res[i][1] = ans.get(i).get(1);
+            }
+return res;
+
+
+
+        
+    }
+}
