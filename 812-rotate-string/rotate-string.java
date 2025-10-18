9292cf77d4405157e80ae4063f3911ab80d9dd8d
DhananiPavan <dhananipavan369@gmail.com>
2025-10-18 21:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (21.65%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        // If lengths are different, rotation is impossible
+        if (s.length() != goal.length()) return false;
+
+        // Concatenate s with itself
+        String combined = s + s;
+
+        // Check if goal is a substring of combined
+        return combined.contains(goal);
+    }
+}
