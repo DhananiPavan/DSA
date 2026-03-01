ebb988470e0c5f38875d54d7904fe94b71b0d6d8
DhananiPavan <dhananipavan369@gmail.com>
2026-03-01 19:53:01 +0530

    Time: 2 ms (99.39%) | Memory: 77.2 MB (86.44%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -8,9 +8,9 @@ class Solution {
             int h=Math.min(height[l],height[r]);
             ans=Math.max(ans,w*h);
             if(height[l]<height[r]){
-              while(l<r && height[l]<=h)l++;
-                
-            }
+              while(l<r && height[l]<=h)l++;//use while loop to skip lesser height compare 
+                                            //previous min height 'h';
+             }
             else{
                 while(l<r && height[r]<=h)r--;
             }
