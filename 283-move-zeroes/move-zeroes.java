e140d7dd35c79aab82d2a21532272647b73201cf
DhananiPavan <dhananipavan369@gmail.com>
2026-03-01 19:31:56 +0530

    Time: 2 ms (90.13%) | Memory: 48 MB (18.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,25 +3,26 @@ class Solution {
         int i=0;
         for(int j=0;j<a.length;j++){
            
-            if(a[j]!=0){
+        //     if(a[j]!=0){
 
-                a[i++]=a[j];
+        //         a[i++]=a[j];
                 
+        //     }
+        // }
+        // while(i<a.length){
+        //     a[i]=0;
+        //     i++;
+        // }
+         if(a[j]!=0)
+               {
+                a[i]=a[j];
+                   if(i!=j)
+                     {
+                      a[j]=0;
+                     }
+                i++;
             }
         }
-        while(i<a.length){
-            a[i]=0;
-            i++;
-        }
-         // if(a[j]!=0)
-            //    {
-            //     a[i]=a[j];
-            //        if(i!=j)
-            //          {
-            //           a[j]=0;
-            //          }
-            //     i++;
-            // }
 
        
         
