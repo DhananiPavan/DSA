d291a443deaba593e46ea329e799ecff614be36f
DhananiPavan <dhananipavan369@gmail.com>
2026-03-07 12:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (95.17%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,16 +1,32 @@
 class Solution {
     public void sortColors(int[] arr) {
-               int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            // After each pass, the largest element moves to the end
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
+     int n = arr.length;
+     int t1=0,t2=0,t3=0;
+     int l=0;
+     while(l<arr.length){
+        if(arr[l]==0)t1++;
+        if(arr[l]==1)t2++;
+        if(arr[l]==2)t3++;
+
+        l++;
+     }
+     for(int i=0;i<arr.length;i++){
+        if(t1!=0){
+            arr[i]=0;
+            t1--;
+            continue;
         }
+        if(t2!=0){
+            arr[i]=1;
+            t2--;
+            continue;
+
+        }
+        arr[i]=2;
+        t3--;
+
+     }
+
+        
     }
 }
