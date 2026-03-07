50993a0d6cc9b18737827f681570e1e3d8c23368
DhananiPavan <dhananipavan369@gmail.com>
2026-03-07 14:52:32 +0530

    Time: 1 ms (11.09%) | Memory: 44.3 MB (96.48%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void nextPermutation(int[] a) {
+     int n=a.length;
+     int ind=-1;
+     for(int i=n-2;i>=0;i--){
+        if(a[i]<a[i+1]){
+            ind=i;
+            break;
+        }
+     } 
+     if(ind==-1){
+        Arrays.sort(a);
+     }
+     else{
+        for(int i=n-1;i>0;i--){
+            if(a[ind]<a[i]){
+             int temp=a[i];
+             a[i] =a[ind];
+             a[ind]=temp;
+             break;
+            }
+        }
+
+        reverse(a,ind+1,n-1);
+     } 
+
+  
+        
+
+       
+    }
+        public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+        
+    
+}
