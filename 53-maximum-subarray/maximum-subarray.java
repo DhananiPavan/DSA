bb30fba9123e2738bb2e981e67c178c9bf12bd0a
DhananiPavan <dhananipavan369@gmail.com>
2026-03-08 18:38:12 +0530

    Time: 1 ms (99.80%) | Memory: 77.4 MB (30.25%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,10 @@ class Solution {
      int max=Integer.MIN_VALUE;
      int sum=0;
      for(int i=0;i<a.length;i++){
-        sum=sum+a[i];
-        max=Math.max(max,sum);
-        sum=(sum<0)?0:sum;
+        sum=sum+a[i];//add every element
+        max=Math.max(max,sum);//check max
+        sum=(sum<0)?0:sum;// if negative  sum occur make it 0
+        
      } 
      return max;  
     }
