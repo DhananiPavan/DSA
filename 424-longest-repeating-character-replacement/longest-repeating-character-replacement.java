58b1de0dd3a64c7dcd0eb1350983f9f57523bed0
DhananiPavan <dhananipavan369@gmail.com>
2026-03-14 19:18:14 +0530

    Time: 7 ms (89.31%) | Memory: 46.5 MB (37.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,29 +3,18 @@ import java.util.*;
 class Solution {
     public int characterReplacement(String s, int k) {
 
-        Map<Character, Integer> map = new HashMap<>();
-
+        int[] freq = new int[26];
         int l = 0;
         int maxFreq = 0;
         int ans = 0;
 
-        for (int r = 0; r < s.length(); r++) {
+        for(int r = 0; r < s.length(); r++) {
 
-            char ch = s.charAt(r);
+            freq[s.charAt(r) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(r) - 'A']);
 
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-
-            maxFreq = Math.max(maxFreq, map.get(ch));
-
-            while ((r - l + 1) - maxFreq > k) {
-
-                char leftChar = s.charAt(l);
-                map.put(leftChar, map.get(leftChar) - 1);
-
-                if (map.get(leftChar) == 0) {
-                    map.remove(leftChar);
-                }
-
+            while((r - l + 1) - maxFreq > k) {
+                freq[s.charAt(l) - 'A']--;
                 l++;
             }
 
@@ -35,3 +24,42 @@ class Solution {
         return ans;
     }
 }
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+
+//         Map<Character, Integer> map = new HashMap<>();
+
+//         int l = 0;
+//         int maxFreq = 0;
+//         int ans = 0;
+
+//         for (int r = 0; r < s.length(); r++) {
+
+//             char ch = s.charAt(r);
+
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+//             maxFreq = Math.max(maxFreq, map.get(ch));
+
+//             while ((r - l + 1) - maxFreq > k) {
+
+//                 char leftChar = s.charAt(l);
+//                 map.put(leftChar, map.get(leftChar) - 1);
+
+//                 if (map.get(leftChar) == 0) {
+//                     map.remove(leftChar);
+//                 }
+
+//                 l++;
+//             }
+
+//             ans = Math.max(ans, r - l + 1);
+//         }
+
+//         return ans;
+//     }
+// }
