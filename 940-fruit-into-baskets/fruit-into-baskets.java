5239415a41add636cd2be56f9aba990800e7aca7
DhananiPavan <dhananipavan369@gmail.com>
2026-03-16 22:53:42 +0530

    Time: 56 ms (29.70%) | Memory: 70.5 MB (51.39%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -6,7 +6,7 @@ class Solution {
     int total=0;
     for(int r=0;r<fruits.length;r++){
       mpp.put(fruits[r],mpp.getOrDefault(fruits[r], 0)+1);
-      total+=1;
+      total+=1;//use r-l+1 instead of total
       while(mpp.size()>2){
         mpp.put(fruits[l],mpp.get(fruits[l])-1);
         total-=1;
