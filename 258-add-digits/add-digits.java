1d749b9f94377bb2d3124e9e7382cc31c638820f
DhananiPavan <dhananipavan369@gmail.com>
2026-03-29 22:27:07 +0530

    Time: 1 ms (98.04%) | Memory: 42.9 MB (18.38%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,6 +1,13 @@
 class Solution {
     public int addDigits(int num) {
         if (num == 0) return 0;
-        return num % 9 == 0 ? 9 : num % 9;
+        return 1+(num-1)%9;
     }
 }
+
+// dr(n)=1+(n−1)mod9;
+
+// dr(11) = 1 + (11 - 1) % 9
+//        = 1 + 10 % 9
+//        = 1 + 1
+//        = 2
