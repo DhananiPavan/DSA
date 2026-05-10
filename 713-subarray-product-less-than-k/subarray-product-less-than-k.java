d1d40a526adebe6007ec2a01209a404b90eee258
DhananiPavan <dhananipavan369@gmail.com>
2026-05-11 00:39:11 +0530

    Time: 4 ms (99.96%) | Memory: 51.2 MB (77.27%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -5,7 +5,7 @@ class Solution {
       int count=0;
       int n=nums.length;
       int pro=1;
-       if(k <= 1) return 0;//most important edge  case
+      if(k <= 1) return 0;//most important edge  case
       for(int r=0;r<n;r++){
         pro*=nums[r];
         while(pro>=k){
