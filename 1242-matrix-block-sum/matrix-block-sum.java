1515e40ad9960d5859488be2adc1f52a135093df
DhananiPavan <dhananipavan369@gmail.com>
2026-05-15 18:43:38 +0530

    Time: 7 ms (68.83%) | Memory: 46.4 MB (93.91%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -3,13 +3,14 @@ class Solution {
         int m=mat.length;
         int n=mat[0].length;
         int[][] prefix=new int[m+1][n+1];
+        //calculate prefix matix
         for(int i=1;i<m+1;i++){
             for(int j=1;j<n+1;j++){
                 prefix[i][j]=mat[i-1][j-1]+prefix[i-1][j]
                 +prefix[i][j-1]-prefix[i-1][j-1];
             }
         }
-
+        //calculate ans matrix using prefix matrix
         int[][] ans=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
