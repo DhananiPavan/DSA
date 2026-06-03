46db863b3b514c883730618cf0845323c50dc2ba
DhananiPavan <dhananipavan369@gmail.com>
2026-06-03 19:51:15 +0530

    Time: 17 ms (21.26%) | Memory: 48.4 MB (57.94%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] ans =new int[nums.length];
-        Arrays.fill(ans, -1);
-        Stack<Integer> stack =new Stack<>();
-        for(int i= 2*nums.length-1;i>=0;i--){
-        while(!stack.isEmpty() && stack.peek()<=nums[i%nums.length]){
-            stack.pop();
+      int[] res = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * nums.length - 1; i >= 0; --i) {
+            while (!stack.empty() && nums[stack.peek()] <= nums[i % nums.length]) {
+                stack.pop();
+            }
+            res[i % nums.length] = stack.empty() ? -1 : nums[stack.peek()];
+            stack.push(i % nums.length);
         }
-        if(!stack.isEmpty()){
-            ans[i%nums.length]=stack.peek();
-        }
-        
-
-
-         stack.push(nums[i%nums.length]);   
-        }
-        return ans;
+        return res;   
 
     }
 }
+// int[] ans =new int[nums.length];
+//         Arrays.fill(ans, -1);
+//         Stack<Integer> stack =new Stack<>();
+//         for(int i= 2*nums.length-1;i>=0;i--){
+//         while(!stack.isEmpty() && stack.peek()<=nums[i%nums.length]){
+//             stack.pop();
+//         }
+//         if(!stack.isEmpty()){
+//             ans[i%nums.length]=stack.peek();
+//         }
+        
+
+
+//          stack.push(nums[i%nums.length]);   
+//         }
+//         return ans;
