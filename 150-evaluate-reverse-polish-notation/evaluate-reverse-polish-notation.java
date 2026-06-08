330591f6454a4873d69e8d3f783f848662279050
DhananiPavan <dhananipavan369@gmail.com>
2026-06-08 19:26:15 +0530

    Time: 3 ms (98.96%) | Memory: 45.1 MB (77.28%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,34 +1,32 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-       Stack<Integer> stack=new Stack<>();
-       for(String s: tokens){
-         if(s.equals("+") ||s.equals("-") || s.equals("*") ||s.equals("/")){
-            int b=stack.pop();
-            int a=stack.pop();
-            switch(s){
-                case "+":
-                          stack.push(a+b);
-                          break;
-                case "-":
-                          stack.push(a-b);
-                          break;
-                case "*":
-                          stack.push(a*b);
-                          break;
-                case "/":
-                          stack.push(a/b);
-                          break; 
-            }
-           
-
-         }
-         else{
-            stack.push(Integer.parseInt(s));
-            }
-          
-       }
-
+        // A primitive array acts as our high-performance stack
+        int[] stack = new int[tokens.length];
+        int top = 0; // Pointer to track the top of the stack
         
-       return stack.pop();
+        for (String s : tokens) {
+            // If the token is longer than 1 character, it MUST be a number (e.g., "11", "-11")
+            // If it is 1 character long, check if it's a digit or an operator
+            char c = s.charAt(0);
+            
+            if (s.length() == 1 && (c == '+' || c == '-' || c == '*' || c == '/')) {
+                // Pop the top two elements out of our primitive stack
+                int b = stack[--top];
+                int a = stack[--top];
+                
+                switch (c) {
+                    case '+': stack[top++] = a + b; break;
+                    case '-': stack[top++] = a - b; break;
+                    case '*': stack[top++] = a * b; break;
+                    case '/': stack[top++] = a / b; break;
+                }
+            } else {
+                // It's a number, parse it and push it directly as a primitive int
+                stack[top++] = Integer.parseInt(s);
+            }
+        }
+        
+        // The last remaining element is our final answer
+        return stack[0];
     }
 }
