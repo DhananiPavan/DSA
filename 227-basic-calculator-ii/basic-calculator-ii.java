9abd360e43039127d271644f76f4a99eb4511767
DhananiPavan <dhananipavan369@gmail.com>
2026-06-08 18:57:08 +0530

    Time: 31 ms (11.35%) | Memory: 48.2 MB (53.09%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,35 +1,34 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack =new Stack<>();
+        Stack<Integer> stack=new Stack<>();
         int num=0;
         char op='+';
         for(int i=0;i<=s.length();i++){
-          char ch=(i==s.length())?0:s.charAt(i);
-          if(Character.isDigit(ch)){
-            num=num*10+(ch-'0');
-          }  
-          else if(ch!=' '){
-            if(op=='+'){
-                stack.push(num);
+            char ch=(i==s.length())?0:s.charAt(i);
+            if(Character.isDigit(ch)){
+                num=num*10+(ch-'0');
             }
-            else if(op=='-'){
-                stack.push(-num);
+            else if(ch!=' '){
+                if(op=='+'){
+                    stack.push(num);
+                }
+                if(op=='-'){
+                    stack.push(-num);
+                }
+                if(op=='*'){
+                    stack.push(stack.pop()*num);
+                }
+                if(op=='/'){
+                    stack.push(stack.pop()/num);
+                }
+                num=0;
+                op=ch;
+                
+                
             }
-            else if(op=='*'){
-                stack.push(stack.pop()*num);
-            }
-            else if(op=='/'){
-                stack.push(stack.pop()/num);
-            }
-            op=ch;
-            num=0;
-          }
         }
         int ans=0;
-        while(!stack.isEmpty()){
-         ans+=stack.pop();
-        }
+        while(!stack.isEmpty()) ans+=stack.pop();
         return ans;
-         
     }
 }
