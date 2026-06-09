f9851fceeb2f51f47f19bdf598870eeddf42e780
DhananiPavan <dhananipavan369@gmail.com>
2026-06-09 12:07:33 +0530

    Time: 1 ms (84.86%) | Memory: 42.8 MB (71.76%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,43 +1,50 @@
 class Solution {
     public String decodeString(String s) {
-
-        Stack<Integer> countStack = new Stack<>();
-        Stack<StringBuilder> stringStack = new Stack<>();
-
-        StringBuilder current = new StringBuilder();
+        Stack<Object> stack = new Stack<>();
         int num = 0;
 
         for (char ch : s.toCharArray()) {
-
             if (Character.isDigit(ch)) {
-
+                // Form the multi-digit number
                 num = num * 10 + (ch - '0');
-
             } else if (ch == '[') {
-
-                countStack.push(num);
-                stringStack.push(current);
-
-                num = 0;
-                current = new StringBuilder();
-
+                // Push the complete number and the bracket onto the stack
+                stack.push(num);
+                stack.push("[");
+                num = 0; // Reset number for the next potential nested block
             } else if (ch == ']') {
-
-                int repeat = countStack.pop();
-                StringBuilder previous = stringStack.pop();
-
-                for (int i = 0; i < repeat; i++) {
-                    previous.append(current);
+                // 1. Pop all strings until we hit the opening bracket '['
+                StringBuilder sb = new StringBuilder();
+                while (!stack.isEmpty() && !stack.peek().equals("[")) {
+                    sb.insert(0, stack.pop());
                 }
-
-                current = previous;
-
+                
+                // Pop the opening bracket '['
+                stack.pop();
+                
+                // 2. The next element on the stack is guaranteed to be the repeat count (Integer)
+                int repeatCount = (Integer) stack.pop();
+                
+                // 3. Repeat the string block
+                StringBuilder repeatedString = new StringBuilder();
+                for (int i = 0; i < repeatCount; i++) {
+                    repeatedString.append(sb);
+                }
+                
+                // Push the newly formed string back onto the stack
+                stack.push(repeatedString.toString());
             } else {
-
-                current.append(ch);
+                // Push normal characters as strings
+                stack.push(String.valueOf(ch));
             }
         }
 
-        return current.toString();
+        // Combine whatever is left in the stack into the final result
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+
+        return result.toString();
     }
 }
