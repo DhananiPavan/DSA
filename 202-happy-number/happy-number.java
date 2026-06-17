8dc81af87a980d7aee5a1bc99c55004a61109ede
DhananiPavan <dhananipavan369@gmail.com>
2026-06-17 20:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (34.40%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isHappy(int n) {
-        // Base cases for single digits
-        if (n == 1 || n == 7) {
+      
+      
+        if(n==1||n==7){
             return true;
         }
-        if (n < 10) {
+        if(n<10){
             return false;
         }
         
-        int sum = 0;
-        int x = n;
-        
-        // Corrected loop to process all digits
-        while (x != 0) {
-            int b = x % 10;
-            sum += b * b; // Accumulate the sum of squares
-            x = x / 10;   // Divide by 10 to move to the next digit
-        }
-        
-        // Recursively call with the new sum
-        return isHappy(sum);
+      int x=n;int sum=0;
+      while(x!=0){
+        int b=x%10;
+        sum+= (int) Math.pow(b,2);
+        x=x/10;
+      }  
+      return isHappy(sum);
     }
 }
