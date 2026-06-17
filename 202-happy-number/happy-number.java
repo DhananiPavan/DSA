6e12ade0b4117333f26a0729568963162024c977
DhananiPavan <dhananipavan369@gmail.com>
2026-06-17 20:43:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (34.40%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isHappy(int n) {
       
-      
+      if(n<10){
         if(n==1||n==7){
             return true;
         }
-        if(n<10){
-            return false;
-        }
+         return false;
+       }
         
       int x=n;int sum=0;
       while(x!=0){
