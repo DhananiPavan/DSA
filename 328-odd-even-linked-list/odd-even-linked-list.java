ad835f3685ca08909c6b8f8a8068a0325beb9bf4
DhananiPavan <dhananipavan369@gmail.com>
2026-06-23 21:23:11 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (16.05%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -2,21 +2,16 @@ class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenHead = even; // store starting point of even list
-
-        // rearrange pointers
-        while (even != null && even.next != null) {
-            odd.next = even.next;     // link odd to next odd
-            odd = odd.next;           // move odd pointer
-
-            even.next = odd.next;     // link even to next even
-            even = even.next;         // move even pointer
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode cur=even;
+        while( even!=null && even.next!=null){
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
         }
-
-        // join odd list with even list
-        odd.next = evenHead;
+        odd.next=cur;
 
         return head;
     }
