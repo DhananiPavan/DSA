071bf441d6124b852ed8e5bcc7e57784b6d4177c
DhananiPavan <dhananipavan369@gmail.com>
2026-06-24 20:13:44 +0530

    Time: 3 ms (99.82%) | Memory: 94.2 MB (78.77%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,35 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
             return true;
-
         }
+
+        // 1. Find the middle of the linked list
         ListNode slow = head;
-        ListNode fst = head;
-        while (fst != null && fst.next != null) {
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-            fst = fst.next.next;
+            fast = fast.next.next;
         }
-        ListNode newNode = null;
-        // if (fst != null) {
-        //     newNode = reverse(slow.next);
 
-        // } else {
-        //     newNode = reverse(slow);
-        // }
-        newNode =reverse(slow);
+        // 2. Reverse the second half in-place (inlined for speed)
+        ListNode prev = null;
+        ListNode curr = slow; // Start reversing right from 'slow'
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // 3. Compare the first and second halves
         ListNode left = head;
-        ListNode right = newNode;
+        ListNode right = prev; // 'prev' is the head of the reversed second half
         while (right != null) {
             if (left.val != right.val) {
                 return false;
@@ -39,19 +34,5 @@ class Solution {
         }
 
         return true;
-
-    }
-
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-        return prev;
-        // 'prev' is now the head of the reversed second half!
     }
 }
