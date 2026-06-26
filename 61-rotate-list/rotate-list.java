cc98eab291929b592bff2147b5b9a22f7ff1b6ff
DhananiPavan <dhananipavan369@gmail.com>
2026-06-26 21:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (76.91%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,34 +10,38 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null|| k==0){
+        if (head == null || head.next == null || k == 0) {
             return head;
         }
 
-       ListNode fst=head;
-       int i =0;
-       ListNode cur=head;
-       while(cur!=null){
-        cur=cur.next;
-        i++;
-       }
-       int n=i;
-       k=k%n;
-       if(k==0)return head;
-       i=1;
-       while(i<=k){
-        fst=fst.next;
-        i++;
-       } 
-       ListNode slow=head;
-       while(fst.next!=null){
-        slow=slow.next;
-        fst=fst.next;
-       }
-       ListNode newNode =slow.next;
-       slow.next=null;
-       fst.next=head;
-       return newNode;
+        ListNode fst = head;
+        ListNode cur = head;
+        int i = 0;
+        //find length
+        while (cur != null) {
+            cur = cur.next;
+            i++;
+        }
+        int n = i;
+        k = k % n;
+        //if k==n then return head
+        if (k == 0)
+            return head;
+        //find previous element
+        i = 1;
+        while (i <= k) {
+            fst = fst.next;
+            i++;
+        }
+        ListNode slow = head;
+        while (fst.next != null) {
+            slow = slow.next;
+            fst = fst.next;
+        }
+        ListNode newNode = slow.next;
+        slow.next = null;
+        fst.next = head;
+        return newNode;
 
     }
 }
