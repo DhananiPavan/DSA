dec781cc7232a83743f315823d21782c003ef829
DhananiPavan <dhananipavan369@gmail.com>
2026-07-21 17:58:43 +0530

    Time: 3 ms (99.56%) | Memory: 46.4 MB (89.72%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,39 +1,27 @@
 class Solution {
     public String reverseWords(String s) {
-       StringBuilder ans= new StringBuilder();
-       StringBuilder word =new StringBuilder();
-       
-      for(char c : s.toCharArray()){
-        if(c==' '){
-            ans.append(reverse(word));
-            ans.append(c);
-            word.setLength(0);
-        }
-        else{
-            word.append(c);
-        }
-      }
-      ans.append(reverse(word));
-      return ans.toString();
-      
-     
-    }
-    // public String reverse(StringBuilder word) {
-    //     return word.reverse().toString();
-    // }
-    private String reverse(StringBuilder word) {
-        int left = 0;
-        int right = word.length() - 1;
+        char[] chars = s.toCharArray();
+        int start = 0;
 
+        for (int end = 0; end <= chars.length; end++) {
+            // Reached space or end of string -> reverse current word
+            if (end == chars.length || chars[end] == ' ') {
+                reverse(chars, start, end - 1);
+                start = end + 1; // Move start pointer to next word
+            }
+        }
+
+        return new String(chars);
+    }
+
+    // Two-pointer swap directly on char array
+    private void reverse(char[] chars, int left, int right) {
         while (left < right) {
-            char temp = word.charAt(left);
-            word.setCharAt(left, word.charAt(right));
-            word.setCharAt(right, temp);
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
             left++;
             right--;
         }
-
-        return word.toString();
     }
-
 }
