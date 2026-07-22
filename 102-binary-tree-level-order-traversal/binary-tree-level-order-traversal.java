a59b273837db609b539128fbe5f2738588e52480
DhananiPavan <dhananipavan369@gmail.com>
2026-07-22 19:53:03 +0530

    Time: 2 ms (19.88%) | Memory: 46.9 MB (22.59%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -16,35 +16,21 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
 
-        List<List<Integer>> ans = new ArrayList<>();
-
-        if (root == null)
-            return ans;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-
-                TreeNode node = queue.poll();
-
-                level.add(node.val);
-
-                if (node.left != null)
-                    queue.offer(node.left);
-
-                if (node.right != null)
-                    queue.offer(node.right);
-            }
-
-            ans.add(level);
+       List<List<Integer>> ans = new ArrayList<>();
+       Queue<TreeNode> q= new LinkedList<>(); 
+       if(root==null) return ans;
+       q.offer(root);
+       while(!q.isEmpty()){
+        int numofnodes=q.size();
+        List<Integer> subtree=new ArrayList<>();
+        for(int i =0;i<numofnodes;i++){
+            if(q.peek().left!=null) q.offer(q.peek().left);
+            if(q.peek().right!=null) q.offer(q.peek().right);
+            subtree.add(q.poll().val);
+            
         }
-
-        return ans;
+        ans.add(subtree);
+       }
+       return ans;
     }
 }
