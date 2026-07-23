69f6cf110955abe428b2e3ffc1d50e5136222158
DhananiPavan <dhananipavan369@gmail.com>
2026-07-23 21:26:11 +0530

    Time: 1 ms (27.28%) | Memory: 43.5 MB (7.60%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -1,52 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public boolean isCousins(TreeNode root, int x, int y) {
-
         Queue<TreeNode> q = new LinkedList<>();
+        if (root == null)
+            return false;
         q.offer(root);
-
+        boolean ans = false;
         while (!q.isEmpty()) {
+            int num = q.size();
+            int check = 0;
+            for (int i = 0; i < num; i++) {
+                TreeNode cur = q.poll();
+                if (cur.val == x || cur.val == y) {
+                    check++;
+                }
+                if (check == 2 && (cur.val == y || cur.val == x)) {
+                    ans = true;
+                }
+                if (cur.left != null && cur.right != null) {
 
-            int size = q.size();
-
-            boolean foundX = false;
-            boolean foundY = false;
-
-            for (int i = 0; i < size; i++) {
-
-                TreeNode node = q.poll();
-
-                if (node.val == x)
-                    foundX = true;
-
-                if (node.val == y)
-                    foundY = true;
-
-                // Check if x and y are siblings
-                if (node.left != null && node.right != null) {
-
-                    int left = node.left.val;
-                    int right = node.right.val;
+                    int left = cur.left.val;
+                    int right = cur.right.val;
 
                     if ((left == x && right == y) ||
-                        (left == y && right == x))
+                            (left == y && right == x))
                         return false;
                 }
+                if (cur.left != null)
+                    q.offer(cur.left);
+                if (cur.right != null)
+                    q.offer(cur.right);
 
-                if (node.left != null)
-                    q.offer(node.left);
-
-                if (node.right != null)
-                    q.offer(node.right);
             }
-
-            if (foundX && foundY)
-                return true;
-
-            if (foundX || foundY)
-                return false;
         }
-
-        return false;
+        return ans;
     }
 }
