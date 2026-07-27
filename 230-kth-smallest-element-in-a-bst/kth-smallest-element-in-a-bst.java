6cdda7de303dbfe1a502698130228868d5fb6c66
DhananiPavan <dhananipavan369@gmail.com>
2026-07-27 19:58:20 +0530

    Time: 1 ms (36.70%) | Memory: 46.7 MB (51.06%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -1,36 +1,51 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         if (root == null) {
+//             return -1;
+//         }
+//         List<Integer> ans = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//          TreeNode cur =q.poll();
+//          ans.add(cur.val);
+//          if(cur.left!=null) q.offer(cur.left);
+//          if(cur.right!=null) q.offer(cur.right);
+
+//         }
+//         Collections.sort(ans);
+//         return ans.get(k-1);
+//     }
+// }
 class Solution {
+    List<Integer> arr = new ArrayList();
+
+    public void traverse(TreeNode root) {
+        if (root == null)
+            return;
+        traverse(root.left);
+        arr.add(root.val);
+        traverse(root.right);
+    }
+
     public int kthSmallest(TreeNode root, int k) {
-        if (root == null) {
-            return -1;
-        }
-        List<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-
-        q.offer(root);
-        while(!q.isEmpty()){
-         TreeNode cur =q.poll();
-         ans.add(cur.val);
-         if(cur.left!=null) q.offer(cur.left);
-         if(cur.right!=null) q.offer(cur.right);
-
-            
-        }
-        Collections.sort(ans);
-        return ans.get(k-1);
+        traverse(root);
+        return arr.get(k - 1);
     }
 }
