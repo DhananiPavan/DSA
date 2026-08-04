93a0046c0ab0424c288eb419dce2d01e702c0aa1
DhananiPavan <dhananipavan369@gmail.com>
2026-08-04 21:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (66.20%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return validate(root, null, null);
+        return validate(root, Long.MIN_VALUE,Long.MAX_VALUE);
     }
 
-    private boolean validate(TreeNode node, Integer min, Integer max) {
+    private boolean validate(TreeNode node, long min, long max) {
         if (node == null) {
             return true;
         }
 
         // Check if the current node violates the range bounds
-        if ((min != null && node.val <= min) || (max != null && node.val >= max)) {
+        if(node.val<=min || node.val>=max){
             return false;
         }
 
