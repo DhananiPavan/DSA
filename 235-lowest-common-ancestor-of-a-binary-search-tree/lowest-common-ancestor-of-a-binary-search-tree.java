b5493231e3f2c1e461f9e233bd162741a4a9050e
DhananiPavan <dhananipavan369@gmail.com>
2026-08-05 18:04:46 +0530

    Time: 6 ms (96.95%) | Memory: 47.8 MB (16.79%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -14,20 +14,31 @@ class Solution {
       return find(root,p,q);
     }
     public TreeNode find(TreeNode root,TreeNode p,TreeNode q){
-        if(root==null || root==p|| root==q){
-            return root;
-        }
-        TreeNode left=find(root.left,p,q);
-        TreeNode right=find(root.right,p,q);
-        if(left==null){
-            return right;
-        }
-        if(right==null){
-            return left;
-        }
-        // if(left!=null && right!=null){
+        // if(root==null || root==p|| root==q){
         //     return root;
         // }
+        // TreeNode left=find(root.left,p,q);
+        // TreeNode right=find(root.right,p,q);
+        // if(left==null){
+        //     return right;
+        // }
+        // if(right==null){
+        //     return left;
+        // }
+        // // if(left!=null && right!=null){
+        // //     return root;
+        // // }
+        // return root;
+        if(root==null){
+            return null;
+        }
+       if (p.val < root.val && q.val < root.val) {
+            return find(root.left, p, q);
+        }
+        
+        else if (p.val > root.val && q.val > root.val) {
+            return find(root.right, p, q);
+        }
         return root;
 
     }
