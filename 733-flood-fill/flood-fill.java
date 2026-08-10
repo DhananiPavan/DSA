c523e90a89feb436ccce007933b8e90915b9eb8b
DhananiPavan <dhananipavan369@gmail.com>
2026-08-10 21:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (62.44%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,43 +1,43 @@
 class Solution {
-    class Node {
-        int first;
-
-        int second;
-
-        Node(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m =image.length;
-        int n =image[0].length;
-       Queue<Node> q = new LinkedList<>();
-        
-        if(image[sr][sc]==color){
+
+        int oldColor = image[sr][sc];
+
+        if (oldColor == color) {
             return image;
         }
-        q.offer(new Node(sr,sc));
-        int s=image[sr][sc];
-        image[sr][sc]=color;
-        int[][] dist={{0,1},{1,0},{-1,0},{0,-1}};
-        while(!q.isEmpty()){
-           Node cur=q.poll();
-           int x= cur.first;
-           int y=cur.second;
-           for(int[] dis:dist){
-            int nx=x+dis[0];
-            int ny=y+dis[1];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && image[nx][ny]==s){
-                image[nx][ny]=color;
-              q.offer(new Node(nx,ny));
-            }
-           }
-        }
 
+        dfs(image, sr, sc, oldColor, color);
 
-     return image;
+        return image;
     }
 
+    private void dfs(int[][] image, int r, int c, int oldColor, int color) {
+
+        // Out of bounds
+        if (r < 0 || r >= image.length ||
+            c < 0 || c >= image[0].length) {
+            return;
+        }
+
+        // Not part of the original connected region
+        if (image[r][c] != oldColor) {
+            return;
+        }
+
+        // Change color
+        image[r][c] = color;
+
+        // Up
+        dfs(image, r - 1, c, oldColor, color);
+
+        // Down
+        dfs(image, r + 1, c, oldColor, color);
+
+        // Left
+        dfs(image, r, c - 1, oldColor, color);
+
+        // Right
+        dfs(image, r, c + 1, oldColor, color);
+    }
 }
