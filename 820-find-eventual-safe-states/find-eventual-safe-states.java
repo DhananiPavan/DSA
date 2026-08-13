1ab76ca6b0071dc12fa2679752ad4544447fd5d0
DhananiPavan <dhananipavan369@gmail.com>
2026-08-13 22:16:41 +0530

    Time: 44 ms (5.16%) | Memory: 57.3 MB (99.44%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,65 +1,47 @@
 class Solution {
-
-    List<Integer> ans = new ArrayList<>();
+    Set<Integer> set = new HashSet<>();
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
-
-        int n = graph.length;
-
-        // 0 = not visited
-        // 1 = currently in DFS path
-        // 2 = safe
-        // 3 = unsafe
-        int[] flags = new int[n];
-
-        for (int i = 0; i < n; i++) {
-
+        int m = graph.length;
+        int n = graph[0].length;
+        List<Integer> ans = new ArrayList<>();
+        boolean[] flags = new boolean[m];
+        for (int i = 0; i < m; i++) {
             if (dfs(i, graph, flags)) {
                 ans.add(i);
+                set.add(i);
+
             }
         }
 
         return ans;
+
     }
 
-    public boolean dfs(int node, int[][] g, int[] f) {
+    public boolean dfs(int node, int[][] g, boolean[] f) {
 
-        // Currently in DFS path -> cycle
-        if (f[node] == 1) {
+        if (f[node] == true) {
             return false;
         }
-
-        // Already calculated
-        if (f[node] == 2) {
+        if(set.contains(node)){
             return true;
         }
-
-        if (f[node] == 3) {
-            return false;
-        }
-
-        // Terminal node
         if (g[node].length == 0) {
-            f[node] = 2;
+              set.add(node);
             return true;
         }
-
-        // Mark as currently visiting
-        f[node] = 1;
-
+        f[node] = true;
+        // boolean p = true;
         for (int i = 0; i < g[node].length; i++) {
-
+            
             if (!dfs(g[node][i], g, f)) {
-
-                // This node leads to a cycle
-                f[node] = 3;
+                f[node]=false;
                 return false;
             }
+
         }
-
-        // All neighbours are safe
-        f[node] = 2;
-
+        f[node]=false;
+        set.add(node);
         return true;
     }
 }
