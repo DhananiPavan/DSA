60e6af4a36d77658d36e185899215edbc598a6ed
DhananiPavan <dhananipavan369@gmail.com>
2026-08-23 21:01:04 +0530

    Time: 8 ms (53.28%) | Memory: 46.9 MB (83.94%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        // First half
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                q1++;
+            } else {
+                sum1 += num.charAt(i) - '0';
+            }
+        }
+
+        // Second half
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                q2++;
+            } else {
+                sum2 += num.charAt(i) - '0';
+            }
+        }
+
+        // Odd number of '?' -> Alice always wins
+        if ((q1 + q2) % 2 == 1) {
+            return true;
+        }
+
+        // Bob can win only if he can make the sums equal
+        return sum1 - sum2 != (q2 - q1) * 9 / 2;
+    }
+}
