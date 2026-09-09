4c6ee1f60ad39c6d5706c2a59c22325c39114776
DhananiPavan <dhananipavan369@gmail.com>
2026-09-09 15:17:33 +0530

    Time: 1 ms (99.36%) | Memory: 42.7 MB (37.18%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -7,27 +7,27 @@ class Solution {
         long ans = 0L;
 
         if (n >= 1000L) {
-            long temp=Math.min(n,999999L);
+            long temp = Math.min(n, 999999L);
             ans += temp - 999L;
         }
 
         if (n >= 1000000L) {
-            long temp=Math.min(n,999999999L);
+            long temp = Math.min(n, 999999999L);
             ans += 2L * (temp - 999999L);
         }
 
-        if (n >=1000000000L) {
-             long temp=Math.min(n,999999999999L);
+        if (n >= 1000000000L) {
+            long temp = Math.min(n, 999999999999L);
             ans += 3L * (temp - 999999999L);
         }
 
         if (n >= 1000000000000L) {
-             long temp=Math.min(n,999999999999999L);
+            long temp = Math.min(n, 999999999999999L);
             ans += 4L * (temp - 999999999999L);
         }
 
         if (n >= 1000000000000000L) {
-            long temp=Math.min(n,999999999999999999L);
+            long temp = Math.min(n, 999999999999999999L);
 
             ans += 5L * (temp - 999999999999999L);
         }
