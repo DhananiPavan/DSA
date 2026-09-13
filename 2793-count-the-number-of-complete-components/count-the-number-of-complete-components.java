cdfab56e95a30e32ec9d3e1fcf88b463e6955714
DhananiPavan <dhananipavan369@gmail.com>
2026-09-13 22:23:09 +0530

    Time: 11 ms (58.26%) | Memory: 47.1 MB (98.53%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,64 +1,45 @@
 class Solution {
-
-    public int countCompleteComponents(int n, int[][] edges) {
-
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+    public int countCompleteComponents(int n1, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n1; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        // Add all edges
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
         }
 
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-
-        boolean[] visited = new boolean[n];
-
+        boolean[] v = new boolean[n1];
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n1; i++) {
+            if (v[i]) continue;
 
-            if (visited[i])
-                continue;
+            // comp[0] = node count (n), comp[1] = total degree sum (e)
+            int[] comp = new int[2];
+            dfs(i, adj, comp, v);
 
-            int[] count = new int[2];
+            int nodes = comp[0];
+            int totalDegrees = comp[1];
 
-            dfs(i, graph, visited, count);
-
-            int nodes = count[0];
-            int edgeCount = count[1] / 2;
-
-            int requiredEdges = nodes * (nodes - 1) / 2;
-
-            if (edgeCount == requiredEdges) {
+            // In a complete graph with 'nodes' vertices, total directed degree sum is nodes * (nodes - 1)
+            if (totalDegrees == nodes * (nodes - 1)) {
                 ans++;
             }
         }
-
         return ans;
     }
 
-    private void dfs(int node,
-                     List<List<Integer>> graph,
-                     boolean[] visited,
-                     int[] count) {
+    public void dfs(int i, List<List<Integer>> adj, int[] comp, boolean[] v) {
+        comp[0]++; // Increment node count (n)
+        v[i] = true;
 
-        visited[node] = true;
-
-        // Count vertices
-        count[0]++;
-
-        // Count edges
-        count[1] += graph.get(node).size();
-
-        for (int next : graph.get(node)) {
-
-            if (!visited[next]) {
-                dfs(next, graph, visited, count);
+        for (int nei : adj.get(i)) {
+            comp[1]++; // Count degree (e)
+            if (!v[nei]) {
+                dfs(nei, adj, comp, v);
             }
         }
     }
