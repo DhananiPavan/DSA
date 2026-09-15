daebddf4fcbd1e2a0ee3913a5341b0d84b8ea257
DhananiPavan <dhananipavan369@gmail.com>
2026-09-15 15:33:54 +0530

    Time: 528 ms (33.63%) | Memory: 76.4 MB (68.81%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,40 @@
+class Solution {
+    class Pair {
+        int node;
+        int weight;
+
+        Pair(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        boolean[] v = new boolean[points.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.weight, b.weight));
+        int sum = 0;
+        int count = 0;
+        // Start with node 0 at cost 0
+        pq.add(new Pair(0, 0));
+        while (!pq.isEmpty()) {
+            Pair cur = pq.poll();
+            int curnode = cur.node;
+            int curw = cur.weight;
+            if (v[curnode]) {
+                continue;
+            }
+            v[curnode] = true;
+            sum += curw;
+            int i = 0;
+            for (int[] arr : points) {
+                if (i != curnode && !v[i]) {
+                    int val = Math.abs(points[curnode][0] - arr[0]) + Math.abs(points[curnode][1] - arr[1]);
+                    pq.add(new Pair(i, val));
+                }
+                i++; // Ensure i increments on every loop iteration
+            }
+        }
+        return sum;
+
+    }
+}
