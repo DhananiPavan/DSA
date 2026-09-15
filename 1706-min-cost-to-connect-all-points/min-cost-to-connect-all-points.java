6cee3c5ddf2f27e83e1e9a5315a1964392d3c763
DhananiPavan <dhananipavan369@gmail.com>
2026-09-15 21:18:07 +0530

    Time: 349 ms (44.61%) | Memory: 98.7 MB (25.63%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,44 +1,58 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-
-        boolean[] visited = new boolean[n];
-
-        PriorityQueue<int[]> pq =
-            new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-
-        pq.add(new int[]{0, 0});
-
-        int sum = 0;
-        int count = 0;
-
-        while (!pq.isEmpty() && count < n) {
-
-            int[] cur = pq.poll();
-
-            int node = cur[0];
-            int weight = cur[1];
-
-            if (visited[node]) {
-                continue;
-            }
-
-            visited[node] = true;
-            sum += weight;
-            count++;
-
-            for (int i = 0; i < n; i++) {
-                if (!visited[i]) {
-
-                    int distance =
-                        Math.abs(points[node][0] - points[i][0])
-                        + Math.abs(points[node][1] - points[i][1]);
-
-                    pq.add(new int[]{i, distance});
-                }
+        List<int[]> edges = new ArrayList<>();
+        int n = points.length;  int cost = 0;
+        DisjointSet ds = new DisjointSet(n);
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                edges.add(new int[]{manDist(points[i], points[j]), i, j});
             }
         }
+        edges.sort(
+            (a,b) -> a[0] - b[0]
+        );
+        for(int[] edge: edges){
+            if(ds.findUP(edge[1]) != ds.findUP(edge[2])){
+                ds.unionBySize(edge[1], edge[2]);
+                cost += edge[0];
+            }
+        }
+        return cost;
+    }
+    private int manDist(int[] fir, int[] sec){
+        int x = Math.abs(fir[0] - sec[0]);
+        int y = Math.abs(fir[1] - sec[1]);
+        return x+y;
+    }
+}
+class DisjointSet{
+    int[] parent;  int[] size;
+    DisjointSet(int node){
+        parent = new int[node];  size = new int[node];
+        for(int i=0; i<node; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUP(int node){
+        if(parent[node] == node) return node;
+        //path compression
+        int ultiP = findUP(parent[node]);
+        parent[node] = ultiP;
+        return ultiP;
+    }
+    void unionBySize(int u, int v){
+        int ult_u = findUP(u);
+        int ult_v = findUP(v);
+        if(ult_u == ult_v) return;
 
-        return sum;
+        if(size[ult_u] < size[ult_v]){
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        }
+        else{
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
+        }
     }
 }
