a9b3cea274ed9069a022d8fc2e9d56dad7bda2b4
DhananiPavan <dhananipavan369@gmail.com>
2026-09-16 15:39:43 +0530

    Time: 1 ms (85.61%) | Memory: 46.8 MB (96.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,61 @@
 class Solution {
-    public int findCircleNum(int[][] c) {
-        boolean[] b=new boolean[c.length];
-        int ans=0;
-        for(int i=0;i<c.length;i++){
-           if(b[i]==false){
-            ans++;
-            dfs(i,c,b);
-           }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
         }
 
-        return ans;
+        return uf.getComponents();
     }
-    public void dfs(int node ,int[][] c,boolean[] b){
-        if(b[node]==true){
-            return;
-        }
-        b[node]=true;
-        for(int i =0;i<c[node].length;i++){
-            if(c[node][i]==1 ){
-              dfs(i,c,b);
+
+    class UnionFind {
+        private int[] parent;
+        private int[] rank;
+        private int components;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            components = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
             }
+        }
+
+        public int find(int i) {
+            if (parent[i] == i) {
+                return i;
+            }
+            return parent[i] = find(parent[i]); // Path compression
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX != rootY) {
+                // Union by rank
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                components--;
+            }
+        }
+
+        public int getComponents() {
+            return components;
         }
     }
 }
