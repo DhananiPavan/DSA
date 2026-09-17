f5d468391ad90e3b72223297a4e1dcb1be640c20
DhananiPavan <dhananipavan369@gmail.com>
2026-09-17 14:54:43 +0530

    Time: 29 ms (71.49%) | Memory: 58.8 MB (22.53%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,24 +1,26 @@
-
+import java.util.Arrays;
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // Max-heap comparing squared Euclidean distance
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
-            (a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1], a[0] * a[0] + a[1] * a[1])
-        );
+        int n = points.length;
+        // dists[i][0] = squared distance
+        // dists[i][1] = original index of the point
+        int[][] dists = new int[n][2];
 
-        for (int[] point : points) {
-            maxHeap.offer(point);
-            if (maxHeap.size() > k) {
-                maxHeap.poll(); // Evict the point farthest from origin
-            }
+        for (int i = 0; i < n; i++) {
+            dists[i][0] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            dists[i][1] = i;
         }
 
-        int[][] result = new int[k][2];
-        while (k > 0) {
-            result[--k] = maxHeap.poll();
+        // Sort rows based on distance (index 0) using primitive array comparison
+        Arrays.sort(dists, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            int originalIndex = dists[i][1];
+            ans[i] = points[originalIndex];
         }
 
-        return result;
+        return ans;
     }
 }
