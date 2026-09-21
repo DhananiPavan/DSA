cb084cbfba93f3061e92b177f912372a83fbca37
DhananiPavan <dhananipavan369@gmail.com>
2026-09-21 15:21:41 +0530

    Time: 7 ms (72.04%) | Memory: 49.7 MB (46.45%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,23 +2,15 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         
-        for (int i = 0; i < strs.length; i++) {
-            int[] count = new int[26];
-            for (char c : strs[i].toCharArray()) {
-                count[c - 'a']++;
-            }
-            
-            // Generate a unique key based on character frequencies
-            StringBuilder sb = new StringBuilder();
-            for (int val : count) {
-                sb.append('#').append(val);
-            }
-            String key = sb.toString();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
             
             if (!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
-            map.get(key).add(strs[i]);
+            map.get(key).add(s);
         }
         
         return new ArrayList<>(map.values());
